0d6a76d5b47387c1a5dba6a1b096af53b1435e88
Varun Gupta <contact@itsvarungupta.com>
2020-06-17 02:15:06 +0530

    signin working

--- app/routes/login.js
+++ app/routes/login.js
@@ -6,9 +6,7 @@ const jwt = require("jsonwebtoken");
 const PatientAuth = require('../models/login');
 // const loginCtrl = require('../controllers/login.controller.js');
 
-/* POST patient signin. */
-// router.post('/patient/signin', loginCtrl.signInPatient);
-
+/* POST patient signup. */
 router.post('/patient/signup',
   [
     check("hid", "Please Enter a Valid HealthCare Id")
@@ -78,6 +76,67 @@ router.post('/patient/signup',
     } catch (err) {
       console.log(err.message);
       res.status(500).send("Error in Saving");
+    }
+  }
+);
+
+/* POST patient signin. */
+router.post(
+  "/patient/signin",
+  [
+    check("password", "Please enter a valid password").isLength({
+      min: 6
+    })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array()
+      });
+    }
+
+    const { hid, password } = req.body;
+    try {
+      let user = await PatientAuth.findOne({
+        hid
+      });
+      if (!user)
+        return res.status(400).json({
+          message: "User Not Exist"
+        });
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch)
+        return res.status(400).json({
+          message: "Incorrect Password !"
+        });
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        "randomString",
+        {
+          expiresIn: 3600
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.status(200).json({
+            token
+          });
+        }
+      );
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({
+        message: "Server Error"
+      });
     }
   }
 );
